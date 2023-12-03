73b035b53bcb005c63b5b2aafd19f5b505e61ffa
okari <nyandika15@gmail.com>
2023-12-03 11:59:44 +0300

    Add routes and the pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './screens/Dashboard';
+import Manage from './screens/data/Manage';
+import Contact from './screens/data/Contact';
+import Invoice from './screens/data/Invoice';
+import Profile from './screens/pages/Profile';
+import Calendar from './screens/pages/Calendar';
+import Kanban from './screens/pages/Kanban';
+import TextEditor from './screens/pages/TextEditor';
+import Faq from './screens/pages/Faq';
+import Pie from './screens/charts/Pie';
+import Line from './screens/charts/Line';
+import Bar from './screens/charts/Bar';
+import Geography from './screens/charts/Geography';
 
 function App() {
   return (
-    <div className="App">
-      okari
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/manage" element={<Manage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/invoice" element={<Invoice />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/calender" element={<Calendar />} />
+        <Route path="/kanban" element={<Kanban />} />
+        <Route path="/text" element={<TextEditor />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/bar" element={<Bar />} />
+        <Route path="/pie" element={<Pie />} />
+        <Route path="/line" element={<Line />} />
+        <Route path="/geography" element={<Geography />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/screens/global/Sidebar.jsx
+++ src/screens/global/Sidebar.jsx
@@ -1,7 +1,39 @@
+import { Icon } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <>
+      <div>
+        <p>ADMIN</p>
+        <Icon />
+      </div>
+      <div>
+        <img src="path/to/image.jpg" alt="Profile" />
+        <p>OKari Nyandika</p>
+        <p>VP fancy admin</p>
+      </div>
+      <div>
+        <Link to="/dashboard">Dashboard</Link>
+        <p>Data</p>
+        <Link to="/">Manage Team</Link>
+        <Link to="/contact">Contacts Information</Link>
+        <Link to="/invoice">Invoices Balance</Link>
+        <Link to="/profile">Profile Form</Link>
+        <p>Pages</p>
+        <Link to="/calendar">Calender</Link>
+        <Link to="/task">Task Bar</Link>
+        <Link to="/text">Text Editor</Link>
+        <Link to="/faq">FAQ page</Link>
+        <p>Charts</p>
+        <Link to="/pie">Pie Chart</Link>
+        <Link to="/bar">Bar Chart</Link>
+        <Link to="/line">Line Chart</Link>
+        <Link to="/geography">Geography Chart</Link>
+      </div>
+    </>
+  );
 }
 
 export default Sidebar;
