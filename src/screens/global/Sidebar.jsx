571d7e1e7156962fd67b716c8ee4c59d9e77696e
okari <nyandika15@gmail.com>
2023-12-06 10:00:12 +0300

    Added icons

--- src/screens/global/Sidebar.jsx
+++ src/screens/global/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Sidebar as SideSec, Menu, MenuItem } from 'react-pro-sidebar';
 import { Link } from 'react-router-dom';
-import { IoMenuOutline } from 'react-icons/io5';
+import { IoMenuOutline, IoHomeOutline, IoPeopleOutline, IoAlbumsOutline, IoReceiptOutline, IoPersonOutline, IoCalendarOutline, IoListOutline, IoCreateOutline, IoHelpCircleOutline, IoPieChartOutline, IoBarChartOutline, IoTrendingUpOutline, IoMapOutline } from 'react-icons/io5';
 import pic from '../../assets/images/pic-001.png';
 import '../../assets/styling/global.css';
 
@@ -11,13 +11,22 @@ function Sidebar() {
   return (
     <SideSec width="240px" collapsed={isCollapsed} backgroundColor="#5672832" transitionDuration={1200} className="section-1">
       <div className="admin-menu">
-        <p>ADMIN</p>
-        <IoMenuOutline onClick={() => setIsCollapsed(!isCollapsed)} />
+        {!isCollapsed ? <p>ADMIN</p> : null }
+        <Menu>
+          <MenuItem>
+            <IoMenuOutline className="ham-icon" onClick={() => setIsCollapsed(!isCollapsed)} />
+          </MenuItem>
+        </Menu>
       </div>
       <div className="profile-section">
         <img className="profile-pic" src={pic} alt="Profile" />
-        <p className="profile-name">OKari Nyandika</p>
-        <p className="profile-role">VP fancy admin</p>
+        {!isCollapsed ? (
+          <>
+            <p className="profile-name">Okari Nyandika</p>
+            <p className="profile-role">VP fancy admin</p>
+          </>
+        ) : null}
+
       </div>
       <Menu
         menuItemStyles={{
@@ -31,24 +40,24 @@ function Sidebar() {
           },
         }}
       >
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/dashboard" />}> Dashboard</MenuItem>
+        <MenuItem icon={<IoHomeOutline />} component={<Link to="/dashboard" />}> Dashboard</MenuItem>
         <p className="nav-item">Data</p>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/manage" />}>Manage Team</MenuItem>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/contact" />}>
+        <MenuItem icon={<IoPeopleOutline />} component={<Link to="/manage" />}>Manage Team</MenuItem>
+        <MenuItem icon={<IoAlbumsOutline />} component={<Link to="/contact" />}>
           Contacts Information
         </MenuItem>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/invoice" />}>Invoices Balance</MenuItem>
+        <MenuItem icon={<IoReceiptOutline />} component={<Link to="/invoice" />}>Invoices Balance</MenuItem>
         <p className="nav-item">Pages</p>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/profile" />}>Profile Form</MenuItem>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/calendar" />}>Calendar</MenuItem>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/task" />}>Task Bar</MenuItem>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/text" />}>Text Editor</MenuItem>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/faq" />}>FAQ page</MenuItem>
+        <MenuItem icon={<IoPersonOutline />} component={<Link to="/profile" />}>Profile Form</MenuItem>
+        <MenuItem icon={<IoCalendarOutline />} component={<Link to="/calendar" />}>Calendar</MenuItem>
+        <MenuItem icon={<IoListOutline />} component={<Link to="/task" />}>Task Bar</MenuItem>
+        <MenuItem icon={<IoCreateOutline />} component={<Link to="/text" />}>Text Editor</MenuItem>
+        <MenuItem icon={<IoHelpCircleOutline />} component={<Link to="/faq" />}>FAQ page</MenuItem>
         <p className="nav-item">Charts</p>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/pie" />}>Pie Chart</MenuItem>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/bar" />}>Bar Chart</MenuItem>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/line" />}>Line Chart</MenuItem>
-        <MenuItem icon={<IoMenuOutline />} component={<Link to="/geography" />}>
+        <MenuItem icon={<IoPieChartOutline />} component={<Link to="/pie" />}>Pie Chart</MenuItem>
+        <MenuItem icon={<IoBarChartOutline />} component={<Link to="/bar" />}>Bar Chart</MenuItem>
+        <MenuItem icon={<IoTrendingUpOutline />} component={<Link to="/line" />}>Line Chart</MenuItem>
+        <MenuItem icon={<IoMapOutline />} component={<Link to="/geography" />}>
           Geography Chart
         </MenuItem>
       </Menu>
